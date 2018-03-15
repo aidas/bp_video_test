d5f70b7070d4b988ea09e5ad33b8b6110e1ec380
aidas <aidas2@gmail.com>
2018-03-15 20:46:56 +0000

    Retrieving videos by tags
    Schema changes + new data retrieval methods in Video Model
    
    Plugging in dashboard view

--- schema.sql
+++ schema.sql
@@ -5,7 +5,7 @@ GRANT CREATE, DELETE, SELECT, INSERT, SELECT, UPDATE ON `bored_panda`.* TO 'bPMa
 USE bored_panda;
 
 CREATE TABLE IF NOT EXISTS `videos` (
-  `id` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `id` varchar(255) NOT NULL,
   `channel_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `published_at` datetime NOT NULL,
   `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
@@ -15,24 +15,26 @@ CREATE TABLE IF NOT EXISTS `videos` (
 
 CREATE TABLE IF NOT EXISTS `video_stats` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `video_id` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `video_id` varchar(255) NOT NULL,
   `scrape_timestamp` int(10) unsigned NOT NULL DEFAULT '0',
   `comment_count` int(10) unsigned NOT NULL DEFAULT '0',
   `dislike_count` int(10) unsigned NOT NULL DEFAULT '0',
   `like_count` int(10) unsigned NOT NULL DEFAULT '0',
   `view_count` int(10) unsigned NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`),
+  KEY `video_id_idx` (`video_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=397 DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `video_tags` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `tag` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `tag` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=940 DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `video_tag_conn` (
   `video_id` varchar(255) NOT NULL,
   `tag_id` int(11) NOT NULL,
-  PRIMARY KEY (`video_id`,`tag_id`)
+  PRIMARY KEY (`video_id`,`tag_id`),
+  CONSTRAINT `video_id` FOREIGN KEY (`video_id`) REFERENCES `videos` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
